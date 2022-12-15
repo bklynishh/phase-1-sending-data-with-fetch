764d0ae0d9cde3388b566dac24de29e505ac6ad1
Hannah Maratea <maratea101@gmail.com>
2022-12-15 12:38:48 -0500

    commit tree f1d5ddc2d0a5340f022aa5a728b6e6cc82a5e4d2

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Add your code here
+const formData = {
+    name: "Hannah Michele Maratea",
+    email: "bklynshh@gmail.com",
+};
+
+fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+
+    })
+    .then(response => respoonse.json())
+    .then(response => console.log(JSON.stringify(response)))
+})
